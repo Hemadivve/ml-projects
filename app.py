d3639e9aafb611123f6f1af0bd1efb26fef1d5d2
Hemadivve <155799103+Hemadivve@users.noreply.github.com>
2024-01-06 11:42:52 +0530

    created ml project

--- app.py
+++ app.py
@@ -13,14 +13,14 @@ X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2)
 classifier=LogisticRegression()
 classifier.fit(X_train,Y_train)
 
-st.title('Crop Recommendation')
+st.title('Crop Recommendation:')
 n=st.number_input('Enter Nitrogen:')
 p=st.number_input('Enter Phosphorus:')
-k=st.number_input('Enter Potassium')
+k=st.number_input('Enter Potassium:')
 t=st.number_input('Enter Temperature')
-h=st.number_input('Enter Humidity')
+h=st.number_input('Enter Humidity:')
 ph=st.number_input('Enter pH:')
-r=st.number_input('Enter Rainfall')
+r=st.number_input('Enter Rainfall:')
 if std.button('Recommend Crop')
 data=[[n,p,k,t,h,ph,r]]
 result=calssifier.predict(data)[0]

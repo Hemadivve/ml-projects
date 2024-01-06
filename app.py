a96ce8027141ba2f9babe20b90fe0c3fb5e03f4a
Hemadivve <155799103+Hemadivve@users.noreply.github.com>
2024-01-06 12:06:45 +0530

    created ml model

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import numpy as numpy
-from sklearn.model_selection import train_train_split
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import streamlit as st
 
 dataset=pd.read_csv('crop.csv')
 X=dataset.iloc[:,:-1].values
-Y=dataset,iloc[:,-1].values
+Y=dataset.iloc[:,-1].values
 
 X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2)
 
@@ -15,7 +15,7 @@ classifier.fit(X_train,Y_train)
 
 st.title('Crop Recommendation:')
 n=st.number_input('Enter Nitrogen:')
-p=st.number_input('Enter Phosphorus:')
+p=st.number_input('Enter Phosphorus:')
 k=st.number_input('Enter Potassium:')
 t=st.number_input('Enter Temperature')
 h=st.number_input('Enter Humidity:')
